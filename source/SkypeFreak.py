3a98e68bab283f59dceeeca5ea0b254fdb9fdead
OsandaMalith <osandajayathissa@gmail.com>
2014-03-12 22:14:04 -0700

    License
    Added a new license

--- source/SkypeFreak.py
+++ source/SkypeFreak.py
@@ -1,3 +1,28 @@
+'''
+Title: SkypeFreak
+Description: A cross platform forensic tool for Skype
+Author: Osanda Malith (@OsandaMalith)
+URL: http://osandamalith.github.io/SkypeFreak/
+Disclaimer:	This tool is meant for ethical (legal) purposes only.
+Notes:	Please note this tool may contain errors, and
+		is provided "as it is". There is no guarantee
+		that it will work on your target server(s), as
+		the code may have to be adapted. 
+		This is to avoid script kiddie abuse as well.
+	
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 import os
 import sqlite3
 import optparse
